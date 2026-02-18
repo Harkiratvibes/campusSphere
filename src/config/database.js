0d56dcc7c4495600e8b7cdaf107818f7752d96c4
Harkirat <sainiharkirat38@gmail.com>
2026-02-19 00:08:37 +0530

    port added

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,

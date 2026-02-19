6768953a5c549d0685b7c12d82ac7d356aa630f6
Harkirat <sainiharkirat38@gmail.com>
2026-02-19 10:03:29 +0530

    database connection timeout added

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,24 @@
 const mysql = require("mysql2/promise");
 
-
 const db = mysql.createPool({
   host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  port: process.env.MYSQLPORT,
   database: process.env.MYSQLDATABASE,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
-db.getConnection()
-  .then(() => console.log("DB Connected ✅"))
-  .catch(err => console.error("DB Error ❌", err));
 
+  connectTimeout: 10000,
+});
+
+db.getConnection()
+  .then(() => console.log("✅ DB Connected"))
+  .catch(err => {
+    console.error("❌ DB Connection Failed:", err.message);
+    process.exit(1);
+  });
 
 module.exports = db;
